fc3ebe84cc95abfa585987c0466d49b07b2ee195
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-02-24 20:01:31 +0200

    Computational part implemented

---
+++ src/Controller.java
@@ -0,0 +1,26 @@
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class Controller{
+    public class PtsRow{
+        private SimpleDoubleProperty x,y;
+        public PtsRow(double x, double y){
+            this.x = new SimpleDoubleProperty(x);
+            this.y = new SimpleDoubleProperty(y);
+        }
+
+        public double getX(){return x.get();}
+        public double getY(){return y.get();}
+        public void setX(double x){this.x.set(x);}
+        public void setY(double y){this.y.set(y);}
+    }
+
+    @FXML private TableView table;
+    @FXML private TableColumn<PtsRow, Double> xPts, yPts;
+    @FXML private TextField regression;
+    @FXML private BorderPane graphPane;
+}
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        double[] xs = {30,28,27,32,33,35};
+        double[] ys = {40,35,34,45,45,48};
+        Solver sv = new Solver(xs, ys);
+        System.out.println("Regression parameters: " + sv.getRegrParams()[0] + ", " + sv.getRegrParams()[1]);
+        System.out.println("Corellation coefficient: " + sv.getCorelCoef());
+        System.out.println("Determination coefficient: " + sv.getDeterCoef());
+    }
+}
