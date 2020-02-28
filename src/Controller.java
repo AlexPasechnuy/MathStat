175c5c15e85148d29b80676e7cafffaf39cc5ac6
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-02-28 20:00:31 +0200

    UI finished. Graph plotting implemented

--- src/Controller.java
+++ src/Controller.java
@@ -21,46 +21,66 @@ import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    public class TabRow{
-        private SimpleDoubleProperty x,y;
-        public TabRow(double x, double y){
+    public class TabRow {
+        private SimpleDoubleProperty x, y;
+
+        public TabRow(double x, double y) {
             this.x = new SimpleDoubleProperty(x);
             this.y = new SimpleDoubleProperty(y);
         }
 
-        public double getX(){return x.get();}
-        public double getY(){return y.get();}
-        public void setX(double x){this.x.set(x);}
-        public void setY(double y){this.y.set(y);}
+        public double getX() {
+            return x.get();
+        }
+
+        public double getY() {
+            return y.get();
+        }
+
+        public void setX(double x) {
+            this.x.set(x);
+        }
+
+        public void setY(double y) {
+            this.y.set(y);
+        }
     }
 
     ObservableList<TabRow> tableData;
+    double minX, maxX, minY, maxY;
     Solver solver;
 
-    @FXML private TableView table;
-    @FXML private TableColumn<TabRow, Double> xPts, yPts;
-    @FXML private TextField xAdd, yAdd;
-    @FXML private BorderPane graphPane;
-    @FXML private TextField corCoef;
-    @FXML private TextField deterCoef;
-    @FXML private TextField regrLine;
+    @FXML
+    private TableView table;
+    @FXML
+    private TableColumn<TabRow, Double> xPts, yPts;
+    @FXML
+    private TextField xAdd, yAdd;
+    @FXML
+    private BorderPane graphPane;
+    @FXML
+    private TextField corCoef;
+    @FXML
+    private TextField deterCoef;
+    @FXML
+    private TextField regrLine;
 
     @Override
-    public void initialize(URL location, ResourceBundle resources){
+    public void initialize(URL location, ResourceBundle resources) {
         tableData = FXCollections.observableArrayList();
-        tableData.addAll(new TabRow(30, 40),new TabRow(27, 35),new TabRow(27, 34),
+        tableData.addAll(new TabRow(30, 40), new TabRow(27, 35), new TabRow(27, 34),
                 new TabRow(32, 45), new TabRow(33, 45), new TabRow(35, 48));
         tableInit();
     }
 
-    private void tableInit(){
+    private void tableInit() {
         table.setItems(tableData);
         xPts.setCellValueFactory(new PropertyValueFactory<>("x"));
         xPts.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
-        xPts.setOnEditCommit(t->updateX(t));
+        xPts.setOnEditCommit(t -> updateX(t));
         yPts.setCellValueFactory(new PropertyValueFactory<>("y"));
         yPts.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
-        yPts.setOnEditCommit(t->updateY(t));
+        yPts.setOnEditCommit(t -> updateY(t));
     }
 
 
@@ -69,7 +89,7 @@ public class Controller implements Initializable {
     }
 
     private void updateY(TableColumn.CellEditEvent<TabRow, Double> t) {
-        tableData .get(t.getTablePosition().getRow()).setY(t.getNewValue());
+        tableData.get(t.getTablePosition().getRow()).setY(t.getNewValue());
     }
 
     @FXML
@@ -83,9 +103,9 @@ public class Controller implements Initializable {
             if (!yAdd.getText().isEmpty()) {
                 y = Double.parseDouble(yAdd.getText());
             }
-            tableData.add(new TabRow(x,y));
+            tableData.add(new TabRow(x, y));
             tableInit();
-        }catch(NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             showError("X or Y are not numbers!");
         }
     }
@@ -98,8 +118,8 @@ public class Controller implements Initializable {
                 throw new NullPointerException();
             }
             tableData.remove(pos);
-           tableInit();
-        }catch(NullPointerException ex){
+            tableInit();
+        } catch (NullPointerException ex) {
             showError("Please, choose point to delete!");
         }
     }
@@ -113,9 +133,13 @@ public class Controller implements Initializable {
 
     @FXML
     private void solveClick(javafx.event.ActionEvent event) {
+        if (tableData.isEmpty()) {
+            showError("There is nothing to solve!!!");
+            return;
+        }
         double[] xs = new double[tableData.size()];
         double[] ys = new double[tableData.size()];
-        for(int i = 0; i < tableData.size(); i++){
+        for (int i = 0; i < tableData.size(); i++) {
             xs[i] = tableData.get(i).getX();
             ys[i] = tableData.get(i).getY();
         }
@@ -126,43 +150,59 @@ public class Controller implements Initializable {
         deterCoef.setText(df.format(solver.getDeterCoef()) + "");
         regrLine.setText(df.format(solver.getRegrParams()[0]) + " + "
                 + df.format(solver.getRegrParams()[1]) + " * x");
+        constructGraphs();
     }
 
-    private void constructGraphs(){
-//        try{
-//            NumberAxis xAxis = new NumberAxis(xFrom,xTo,(xTo-xFrom)/20);
-//            NumberAxis yAxis = new NumberAxis(fw.getMinY(),fw.getMaxY(),(fw.getMaxY()- fw.getMinY())/20);
-//            xAxis.setLabel("x");
-//            yAxis.setLabel("y");
-//            LineChart<Number,Number> newChart = new LineChart<>(xAxis,yAxis);
-//            newChart.setCreateSymbols(false);
-//            graphPane.getChildren().clear();
-//            graphPane.getChildren().add(newChart);
-//            double step = (xTo-xFrom) / 100;
-//            XYChart.Series<Number,Number> fSeries = new XYChart.Series<>();
-//            fSeries.setName("f(x)");
-//            XYChart.Series<Number,Number> gSeries = new XYChart.Series<>();
-//            gSeries.setName("g(x)");
-//            for(double x = xFrom;x <= xTo;x+=step) {
-//                fSeries.getData().add(new XYChart.Data<>(x, f.solve(x)));
-//                gSeries.getData().add(new XYChart.Data<>(x, g.solve(x)));
-//            }
-//            fSeries.getData().add(new XYChart.Data<>(xTo,f.solve(xTo)));
-//            gSeries.getData().add(new XYChart.Data<>(xTo,g.solve(xTo)));
-//            newChart.getData().addAll(fSeries, gSeries);
-//            for(int i = 0; i < resPts.size(); i++){
-//                XYChart.Series<Number,Number> newSeries = new XYChart.Series<>();
-//                newSeries.getData().add(new XYChart.Data<>(resPts.get(i).getX(), resPts.get(i).getY()));
-//                newSeries.setName("Intersection" + (i+1));
-//                newChart.getData().add(newSeries);
-//            }
-//            graphPane.getChildren().clear();
-//            graphPane.setCenter(newChart);
-//        }catch (Exception ex){
-//            Alert alert = new Alert(Alert.AlertType.ERROR);
-//            alert.setTitle("Error");
-//            alert.setHeaderText("Error");
-//            alert.showAndWait();
-//        }
+    private void getGraphBounds() {
+        minY = maxY = tableData.get(0).getY();
+        minX = maxX = tableData.get(0).getX();
+        for (int i = 1; i < tableData.size(); i++) {
+            double x = tableData.get(i).getX();
+            double y = tableData.get(i).getY();
+            if (x < minX) {
+                minX = x;
+            } else if (x > maxX) {
+                maxX = x;
+            }
+            if (y < minY) {
+                minY = y;
+            } else if (y > maxY) {
+                maxY = y;
+            }
+        }
+    }
+
+    private void constructGraphs() {
+        getGraphBounds();
+        double xRange = maxX - minX;
+        double yRange = maxY - minY;
+        try {
+            NumberAxis xAxis = new NumberAxis(minX - xRange / 10, maxX + xRange / 10, xRange / 20);
+            NumberAxis yAxis = new NumberAxis(minY - yRange / 10, maxY + yRange / 10, yRange / 20);
+            xAxis.setLabel("x");
+            yAxis.setLabel("y");
+            LineChart<Number, Number> newChart = new LineChart<>(xAxis, yAxis);
+            newChart.setCreateSymbols(true);
+            graphPane.getChildren().clear();
+            graphPane.getChildren().add(newChart);
+            XYChart.Series<Number, Number> scPlot = new XYChart.Series<>();
+            scPlot.setName("scatterplot");
+            XYChart.Series<Number, Number> regrLine = new XYChart.Series<>();
+            regrLine.setName("regression line");
+            for (TabRow row : tableData) {
+                scPlot.getData().add(new XYChart.Data<>(row.getX(), row.getY()));
+            }
+            for (double x = minX; x <= maxX; x += xRange / 50) {
+                regrLine.getData().add(new XYChart.Data<>(x, solver.getRegrParams()[0] + solver.getRegrParams()[1] * x));
+            }
+            newChart.getData().addAll(scPlot, regrLine);
+            graphPane.getChildren().clear();
+            graphPane.setCenter(newChart);
+        } catch (Exception ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error");
+            alert.showAndWait();
+        }
     }
 }
